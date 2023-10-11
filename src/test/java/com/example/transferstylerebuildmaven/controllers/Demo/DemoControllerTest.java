182d03231274c49eb971397b00617dd928ebd8e8
Max <Tru.coder@yandex.ru>
2023-10-11 21:35:42 +0400

    Cover controllers with integration tests

--- src/test/java/com/example/transferstylerebuildmaven/controllers/Demo/DemoControllerTest.java
+++ src/test/java/com/example/transferstylerebuildmaven/controllers/Demo/DemoControllerTest.java
@@ -1,33 +1,15 @@
 package com.example.transferstylerebuildmaven.controllers.Demo;
 
 
-import com.example.transferstylerebuildmaven.models.user.Role;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.MockitoAnnotations;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.request.RequestPostProcessor;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static com.example.transferstylerebuildmaven.models.user.Role.USER;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -35,6 +17,20 @@ class DemoControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Test
+    public void testNonSecuredHelloEndpoint() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/api/v1/auth/demo-controller")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("Hello from non-secured end-point"));
+    }
+    @Test
+    public void testRejectToSaySecuredHelloEndpoint() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/non-auth/demo-controller")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isForbidden());
+    }
 
     @Test
     @WithMockUser(username = "testUser", roles = "USER")
@@ -45,17 +41,32 @@ class DemoControllerTest {
                 .andExpect(MockMvcResultMatchers.content().string("Hello from secured end-point"));
     }
 
-
-
     @Test
-
-    public void testNonSecuredHelloEndpoint() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders
-                        .get("/api/v1/auth/demo-controller")
+    @WithMockUser(username = "testUser", roles = "USER")
+    public void testGetUsernameUser() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/demo-controller/info")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().string("Hello from non-secured end-point"));
+                .andExpect(MockMvcResultMatchers.content().string("testUser"));
     }
+
+    @Test
+    @WithMockUser(username = "testAdmin", roles = "ADMIN")
+    public void testGetUsernameAdmin() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/demo-controller/info")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("testAdmin"));
+    }
+    @Test
+    @WithMockUser(username = "testManager", roles = "MANAGER")
+    public void testGetUsernameManager() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/demo-controller/info")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("testManager"));
+    }
+
 
 
 }
---
+++ src/test/java/com/example/transferstylerebuildmaven/controllers/auth/AuthenticationControllerTest.java
@@ -0,0 +1,150 @@
+package com.example.transferstylerebuildmaven.controllers.auth;
+
+import com.example.transferstylerebuildmaven.models.user.User;
+import com.example.transferstylerebuildmaven.repositories.UserRepository;
+import com.example.transferstylerebuildmaven.requests.AuthenticationRequest;
+import com.example.transferstylerebuildmaven.requests.RegisterRequest;
+import com.example.transferstylerebuildmaven.respones.AuthenticationResponse;
+import com.example.transferstylerebuildmaven.services.AuthenticationService;
+import com.example.transferstylerebuildmaven.services.JwtService;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.*;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+import java.io.IOException;
+
+import static com.example.transferstylerebuildmaven.models.user.Role.ADMIN;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class AuthenticationControllerTest {
+
+    @Autowired
+    private AuthenticationService authenticationService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private JwtService jwtService;
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testRegister() throws JsonProcessingException {
+        RegisterRequest registerRequest =  RegisterRequest.builder()
+                .username("adminious")
+                .firstname("firstname")
+                .lastname("lastname")
+                .email("admin@mail.com")
+                .password("password")
+                .role(ADMIN)
+                .build();
+
+
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<RegisterRequest> entity = new HttpEntity<>(registerRequest, headers);
+
+        ResponseEntity<String> response = restTemplate.exchange("http://localhost:" + port + "/api/v1/auth/register", HttpMethod.POST, entity, String.class);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode responseJson = objectMapper.readTree(response.getBody());
+        String accessTokenFromResponse = responseJson.get("access_token").asText();
+        String refreshTokenFromResponse = responseJson.get("refresh_token").asText();
+
+        assertEquals(registerRequest.getUsername(), jwtService.extractUsername(accessTokenFromResponse));
+        assertEquals(registerRequest.getUsername(), jwtService.extractUsername(refreshTokenFromResponse));
+
+    }
+
+    @Test
+    void testCreateUserThenAuthenticate() throws Exception {
+        User user = User
+                .builder()
+                .username("username")
+                .firstname("firstname")
+                .lastname("lastname")
+                .email("admin@mail.com")
+                .password(passwordEncoder.encode("password"))
+                .role(ADMIN)
+                .build();
+
+        userRepository.save(user);
+
+        // see method test Register
+        AuthenticationRequest authenticationRequest = AuthenticationRequest
+                .builder()
+                .username(user.getUsername())
+                .password("password")
+                .build();
+
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<AuthenticationRequest> entity = new HttpEntity<>(authenticationRequest, headers);
+
+        ResponseEntity<String> response = restTemplate.exchange("http://localhost:" + port + "/api/v1/auth/authenticate", HttpMethod.POST, entity, String.class);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode responseJson = objectMapper.readTree(response.getBody());
+        String accessTokenFromResponse = responseJson.get("access_token").asText();
+        String refreshTokenFromResponse = responseJson.get("refresh_token").asText();
+
+        assertEquals(authenticationRequest.getUsername(), jwtService.extractUsername(accessTokenFromResponse));
+        assertEquals(authenticationRequest.getUsername(), jwtService.extractUsername(refreshTokenFromResponse));
+
+
+    }
+
+    @Test
+    public void testRefreshToken_Success() throws IOException {
+
+
+
+    }
+    private AuthenticationResponse saveUserToDatabaseAndGetResponse(){
+
+
+        return null;
+    }
+
+    private static String asJsonString(Object obj) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+            return objectMapper.writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
